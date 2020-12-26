8fe020d5727bcb4ed04ecb30c358a97a824c6a85
donovanherion <donovanherion@gmail.com>
2020-12-26 18:07:33 +0100

    final working version after tutorial before deployment on amaclone project

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCtE8MHxb8c4EdVWPiC-AAzI_y4gASMvZg",
-  authDomain: "clone-e2868.firebaseapp.com",
-  projectId: "clone-e2868",
-  storageBucket: "clone-e2868.appspot.com",
-  messagingSenderId: "1071216114223",
-  appId: "1:1071216114223:web:b439be70617e211f4ba4da",
-  measurementId: "G-TRJ3C0DQ3S",
+  apiKey: "AIzaSyAKFbDyjDv7yOfUoZwCb78-9KuQ8wA5-lM",
+  authDomain: "amaclone-673b4.firebaseapp.com",
+  projectId: "amaclone-673b4",
+  storageBucket: "amaclone-673b4.appspot.com",
+  messagingSenderId: "968180254889",
+  appId: "1:968180254889:web:8695bc467a234384db41b8",
+  measurementId: "G-JTCZCB0FRP",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
